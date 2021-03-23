525e0f75f746e4c17707fa000f9f748959c7086b
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-23 17:07:58 -0300

    Create insert-random.sql

---
+++ database/insert-random.sql
@@ -0,0 +1,7 @@
+INSERT INTO emplado (id, rut, nombres, apellido_pat, apellido_mat, imagen)
+VALUES(NULL, '19.243.198-0', 'Francisco Ignacio', 'Berwart', 'Ramirez', 
+'https://avatars.githubusercontent.com/u/58098908?s=460&u=d72ba81012ad1946faeaa320214a487dbfc6c15f&v=4'),
+(NULL, '18.769.595-3', 'Francisco Ignacio', 'Torres', 'Sierra',
+'https://avatars.githubusercontent.com/u/58098908?s=460&u=d72ba81012ad1946faeaa320214a487dbfc6c15f&v=4')
+
+
