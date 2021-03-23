953cd035262f432239c2ec100bd17ac18e59d0f6
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-23 17:02:30 -0300

    Run Build

---
+++ backend/build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Despues esta base de datos se irá a un hoster cloud, asique cambiaran las keys😋
+exports.default = {
+    database: {
+        host: "localhost",
+        user: "root",
+        password: "admin",
+        database: "mantenedor_empleados",
+    },
+};
